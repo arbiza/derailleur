4db759411c038a666b7b76974361960d893ed6e4
Lucas Arbiza <lucas@pop-rs.rnp.br>
2014-08-27 19:28:31 -0300

    Changed the use of namespace in Controller class source file and methods implementation moved to the source.
    Instead of using namespace {} in the source file, I changed to derailleur::Controller...

--- controller/controller.cpp
+++ controller/controller.cpp
@@ -1,12 +1,41 @@
-// Implements lslslslsls
+
+/**
+ *  File: controller.cpp
+ *
+ *  Copyright (c) 2014 Lucas Arbiza <lucas.arbiza@gmail.com>
+ * 
+ *  This file is an implementation of the class(es) defined in
+ *  controller.h header file.
+ *
+ *  See the header file for documentation and license information.
+ *
+ **/
+
 
 #include "controller.h"
 
-namespace derailleur {
-    
-    void Controller::teste()
-    { 
-	std::cout << "OK!" << std::endl; 
-    }
 
+
+void derailleur::Controller::message_callback(
+    fluid_base::OFConnection *ofconn, 
+    uint8_t type, void *data, size_t len)
+{
+     std::cout << "message" << std::endl;
+}
+
+
+
+void derailleur::Controller::connection_callback(
+    fluid_base::OFConnection *ofconn, 
+    fluid_base::OFConnection::Event type)
+{
+    // what?
+}
+
+
+
+// TODO: remove it!    
+void derailleur::Controller::teste()
+{ 
+    std::cout << "OK!" << std::endl; 
 }
--- controller/controller.h
+++ controller/controller.h
@@ -1,5 +1,29 @@
-#ifndef __CONTROLLER_HH__ 
-#define __CONTROLLER_HH__
+
+/**
+ *  File: controller.h
+ *
+ *  Copyright (c) 2014 Lucas Arbiza <lucas.arbiza@gmail.com>
+ * 
+ *  This program is free software: you can redistribute it and/or 
+ *  modify it under the terms of the GNU Lesser General Public 
+ *  License as published by the Free Software Foundation, either 
+ *  version 3 of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public 
+ *  License along with this program.  If not, see 
+ *  http://www.gnu.org/licenses/.
+ *
+ **/
+
+
+
+#ifndef _CONTROLLER_H_
+#define _CONTROLLER_H_
 
 #include <fluid/OFServer.hh>
 
@@ -8,46 +32,51 @@
 //#include <list>
 
 namespace derailleur {
-    
-class Controller : public fluid_base::OFServer {
 
-    bool running;
+// This class inherits from fluid_base::OFServer ans implements
+// Derailleur controller functionalities.
+// Usage: ...     
+class Controller : public fluid_base::OFServer {
     
 public:
 
     Controller(const char* address = "0.0.0.0", const int port = 6653,
-			   const int n_workers = 4, bool secure =
-			   false) : fluid_base::OFServer(address,
-			   port, n_workers, secure,
-			   fluid_base::OFServerSettings().supported_version(1).
-			   supported_version(4).keep_data_ownership(false))
-			   { this->running = true; }
+	       const int n_workers = 4, bool secure =
+	       false) 
+	: fluid_base::OFServer(address,
+			       port, n_workers, secure,
+			       fluid_base::OFServerSettings().supported_version(1).
+			       supported_version(4).keep_data_ownership(false))
+    { 
+	this->running_ = true; 
+    }
 
     // void register_for_event(fluid_base::Application* app, int
     // event_type) {
 //
     //  }
 
-    void stop() { this->running = false; fluid_base::OFServer::stop();
-        }
-
-
+    // Method called when a new message arrives
+    // @param 
     virtual void message_callback(fluid_base::OFConnection* ofconn,
 				  uint8_t type, void* data, size_t
-				  len) { std::cout << "message" <<
-				  std::endl; }
+				  len);
+    
 
     virtual void connection_callback(fluid_base::OFConnection* ofconn,
 				     fluid_base::OFConnection::Event
-				     type) {
-
-    }
+				     type);
 
 
     void teste();
+
+private:
+    bool running_;
     
 };
     
 } // namespace derailleur
 
-#endif
+
+#endif // _CONTROLLER_H_
+
