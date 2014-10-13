6762ea590e0b6a00a782815b3ceabd2f1daacfdb
Lucas Arbiza <lucas.arbiza@gmail.com>
2014-10-12 23:45:16 -0300

    I just pasted a flow configuration from libfluid examplos in main (test) file.

--- apps/test/main.cpp
+++ apps/test/main.cpp
@@ -48,24 +48,61 @@ public:
 
           std::cout << "MAC: " << s->get_mac_address() << std::endl;
           
+//           fluid_msg::of13::FlowMod fm;
+//           //fm.xid(pi.xid());
+//           fm.cookie(30);
+//           fm.cookie_mask(0xffffffffffffffff);
+//           fm.table_id(0);
+//           fm.command(fluid_msg::of13::OFPFC_ADD);
+//           fm.idle_timeout(0);
+//           fm.hard_timeout(0);
+//           fm.priority(100);
+//           fm.buffer_id( 0xffffffff );
+//           fm.out_port( fluid_msg::of13::OFPP_ANY );
+//           fm.out_group( fluid_msg::of13::OFPG_ANY );
+//           fm.flags(0);
+//           of13::EthSrc fsrc(((uint8_t*) &src) + 2);
+//           of13::EthDst fdst(((uint8_t*) &dst) + 2);
+//           fm.add_oxm_field(fsrc);
+//           fm.add_oxm_field(fdst);
+//           of13::OutputAction act(out_port, 1024);
+          
+          
+          /* Example from libfluid MsgApps.hh */
+          uint8_t* buffer;
+          /*You can also set the message field using
+          class methods which have the same names from
+          the field present on the specification*/
           fluid_msg::of13::FlowMod fm;
           //fm.xid(pi.xid());
-          fm.cookie(30);
+          fm.cookie(123);
           fm.cookie_mask(0xffffffffffffffff);
           fm.table_id(0);
           fm.command(fluid_msg::of13::OFPFC_ADD);
-          fm.idle_timeout(0);
-          fm.hard_timeout(0);
+          fm.idle_timeout(5);
+          fm.hard_timeout(10);
           fm.priority(100);
-          fm.buffer_id( 0xffffffff );
-          fm.out_port( fluid_msg::of13::OFPP_ANY );
-          fm.out_group( fluid_msg::of13::OFPG_ANY );
+          fm.buffer_id(0xffffffff);
+          fm.out_port(0);
+          fm.out_group(0);
           fm.flags(0);
-          of13::EthSrc fsrc(((uint8_t*) &src) + 2);
-          of13::EthDst fdst(((uint8_t*) &dst) + 2);
+          //of13::EthSrc fsrc(((uint8_t*) &src) + 2);
+          //of13::EthDst fdst(((uint8_t*) &dst) + 2);
           fm.add_oxm_field(fsrc);
           fm.add_oxm_field(fdst);
           of13::OutputAction act(out_port, 1024);
+          of13::ApplyActions inst;
+          inst.add_action(act);
+          fm.add_instruction(inst);
+          buffer = fm.pack();
+          ofconn->send(buffer, fm.length());
+          OFMsg::free_buffer(buffer);
+          of13::Match m;
+          of13::MultipartRequestFlow rf(2, 0x0, 0, of13::OFPP_ANY, of13::OFPG_ANY,
+          0x0, 0x0, m);
+          buffer = rf.pack();
+          ofconn->send(buffer, rf.length());
+          OFMsg::free_buffer(buffer);
           
      }
 
