0b72e5993cf148adda15d088a8c11ae10719a172
Lucas Arbiza <lucas.arbiza@gmail.com>
2014-10-17 01:10:02 -0300

    Recreating Flow class.
    At first I am defining class interface; I am also migrating the code I
    already wrote to the class methods.

--- controller/flow.hpp
+++ controller/flow.hpp
@@ -20,113 +20,64 @@
 #ifndef _FLOW_H
 #define _FLOW_H
 
-#include <cstdlib>
-#include <cstring>
-#include <string>
-#include <sstream>
-#include <iomanip>
-#include <vector>
+# include <vector>
 
-#include <fluid/of10msg.hh>
-#include <fluid/of13msg.hh>
+
+
+// forward declarations
+namespace fluid_msg {
+class fluid_msg::FlowModCommon;
+class fluid_msg::PacketInCommon;
+class fluid_msg::Action;
+}
+
 
 
 namespace derailleur {
 
 
-/**
- * Namespace flow is a new abstraction layer to handle flow objects from libguid.
- * Functions provided handle flows of any OpenFlow version transparently.
- */
-namespace flow {
-
-
 unsigned const MAC_bytes_length = 6;
+
+/**
+ * enum flow_commands provides the same enumerations than of13 and of10
+ * namespaces, but using this enum developers do not need to use an specific
+ * enumerator for each flow version.
+ */
+enum flow_commands {
+     ADD = 0,            // Add a new flow.
+     MODIFY = 1,         // Modify all matching flows.
+     MODIFY_STRICT = 2, // Modify entry strictly matching wildcards and priority.
+     DELETE = 3,         // Delete all matching flows.
+     DELETE_STRICT = 4  // Delete entry strictly matching wildcards and priority.
+};
+
 
 /**
  * FlowTable stores Flow class derived objects.
  */
-typedef std::vector<fluid_msg::FlowModCommon*> FlowTable;
+typedef std::vector<Flow*> FlowTable;
 
 
+class Flow {
 
-/**
- * Create a basic flow with some default values set. The flow is created
- * according OpenFlow version.
- *
- * @param version OpenFlow version
- * @param command OpenFlow command enumerated in ofp_flow_mod_command (described
- *                      bellow); I used enumerator from of13 because it is the 
- *                      same as of10.
- * @return fluid_msg::FlowModCommon* pointer; you need to static_cast the
- *              returning pointer to a pointer of flow mod of the version you 
- *              are using; you may also use auto ptr* (recommended).
- *
- * enum ofp_flow_mod_command {
- *      OFPFC_ADD = 0,          // New flow.
- *      OFPFC_MODIFY = 1,       // Modify all matching flows.
- *      OFPFC_MODIFY_STRICT = 2,// Modify entry strictly matching wildcards
- *                              // and priority.
- *      OFPFC_DELETE = 3,       // Delete all matching flows.
- *      OFPFC_DELETE_STRICT = 4,// Delete entry strictly matching wildcards and
- *                              //priority.
- * };
- */
+public:
+     /**
+      * Constructor creates a flow with default values for fields common to
+      * OpenFlow message versions.
+      */
+     Flow();
+     
+     /**
+      * Sets the command this flow executes on switch(es).
+      */
+     bool set_command_ADD ( uint16_t priority );
+     bool set_command_MODIFY ();
+     bool set_command_MODIFY_STRICT ()
 
-static fluid_msg::FlowModCommon* create_flow_from_packet_in (
-     uint8_t version, 
-     const fluid_msg::of13::PacketIn* packet_in )
-{
-
-     fluid_msg::FlowModCommon* flow;
-
-     // OpenFlow 1.3 messages
-     if ( version == fluid_msg::of13::OFP_VERSION ) {
-          
-          fluid_msg::of13::FlowMod* flow13 = new fluid_msg::of13::FlowMod;
-          fluid_msg::of13::PacketIn* packet_in = new fluid_msg::of13::PacketIn();
-                                   
-          packet_in->unpack ( event->get_data() );
-          
-          flow13->xid( packet_in->xid() );
-          
-          
-          // //fm.xid(pi.xid());
-          // fm.cookie ( 123 );
-          // fm.cookie_mask ( 0xffffffffffffffff );
-          // fm.table_id ( 0 );
-          // fm.command ( fluid_msg::of13::OFPFC_ADD );
-          // fm.idle_timeout ( 5 );
-          // fm.hard_timeout ( 10 );
-          // fm.priority ( 100 );
-          // fm.buffer_id ( 0xffffffff );
-          // fm.out_port ( 0 );
-          // fm.out_group ( 0 );
-          // fm.flags ( 0 );
-          // //of13::EthSrc fsrc(((uint8_t*) &src) + 2);
-          // //of13::EthDst fdst(((uint8_t*) &dst) + 2);
-          // fm.add_oxm_field ( fsrc );
-          // fm.add_oxm_field ( fdst );
-          // of13::OutputAction act ( out_port, 1024 );
-          // of13::ApplyActions inst;
-          // inst.add_action ( act );
-          // fm.add_instruction ( inst );
-
-          flow = flow13;
-     }
-     // OpenFlow 1.0 messages
-     else if ( version == fluid_msg::of10::OFP_VERSION ) {
-
-          fluid_msg::of10::FlowMod* flow10 = new fluid_msg::of10::FlowMod;
-
-          flow = flow10;
-     }
-     else {
-          flow = nullptr;
-     }
-
-     return flow;
-}
+private:
+     fluid_msg::FlowModCommon* flow_mod_;
+     fluid_msg::Action* action_;
+};
 
 
 
@@ -189,7 +140,7 @@ static uint8_t* convert_MAC_to_bits ( const std::string* mac )
 } // namespace util
 
 
-} // namespace flow
+
 } // namespace derailleur
 
 #endif // _FLOW_H
