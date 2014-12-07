10682df936238111e2a89c0e3bc34c2ef413ee68
Lucas Arbiza <lucas.arbiza@gmail.com>
2014-12-07 17:56:43 -0200

    Moved derailleur.hpp to the controller directory; it will be installed on the system through make install

---
+++ controller/derailleur.hpp
@@ -0,0 +1,33 @@
+
+/**
+ *  File: derailleur.hpp
+ *
+ *  Copyright (c) 2014 Lucas Arbiza <lucas.arbiza@gmail.com>
+ * 
+ *  Distributed under the Lesser General Public License v3. 
+ *  There is a copy of the license distributed with this software. It is also
+ *  available at <https://www.gnu.org/licenses/lgpl.html>
+ *
+ **/
+
+
+
+#ifndef _DERAILLEUR_HPP_
+#define _DERAILLEUR_HPP_
+
+// This file is the only one anyone which will use derailleur controller
+// should include. It includes the header files needed for applications.
+
+#include <fluid/of10msg.hh>
+#include <fluid/of13msg.hh>
+
+#include "controller/controller.hpp"
+#include "controller/application.hpp"
+#include "controller/event.hpp"
+#include "controller/switch.hpp"
+#include "controller/util.hpp"
+#include "controller/log.hpp"
+
+
+
+#endif // _DERAILLEUR_HPP_
